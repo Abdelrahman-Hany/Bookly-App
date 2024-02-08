3bf246b0587a1bf800602f1a3db23b8b21e8ed52
Abdelrahman-Hany <abdelrahman.hany013@gmail.com>
2024-02-08 17:18:25 +0200

    first commit

---
+++ lib/features/splash/presentation/views/widgets/splash_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});//we must make this constructor constant to improve app performance
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
