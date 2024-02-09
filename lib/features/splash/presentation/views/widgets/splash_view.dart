19467ba4a3a10ebdbbc7a20114e0d886d300116c
Abdelrahman-Hany <abdelrahman.hany013@gmail.com>
2024-02-09 19:12:01 +0200

    adding sliding animation to Splash View

--- lib/features/splash/presentation/views/widgets/splash_view.dart
+++ lib/features/splash/presentation/views/widgets/splash_view.dart
@@ -1,8 +1,41 @@
 import 'package:bookly_app/core/utlis/assets.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
-  const SplashViewBody({super.key});//we must make this constructor constant to improve app performance
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  //this must be used with animation to handle the rate of change of values
+//we must make this constructor constant to improve app performance
+
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();//to turn off controller
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +44,10 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-        const SizedBox(height: 4,),
-        const Text('Read Free Books',
-        textAlign: TextAlign.center,
+        const SizedBox(
+          height: 4,
         ),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
