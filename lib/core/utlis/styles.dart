473e53149d8f534fd2bd5bc6aad20def9ad62986
Abdelrahman-Hany <abdelrahman.hany013@gmail.com>
2024-02-11 17:18:41 +0200

    add font family to the app

--- lib/core/utlis/styles.dart
+++ lib/core/utlis/styles.dart
@@ -4,6 +4,5 @@ abstract class Styles {
   static const titleMedium = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.w600,
-
   );
 }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:bookly_app/core/utlis/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/custom_listview.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
 
