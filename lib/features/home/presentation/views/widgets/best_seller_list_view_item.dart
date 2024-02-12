2ad3433983df3c80d46328b9fbdae072d02c998b
Abdelrahman-Hany <abdelrahman.hany013@gmail.com>
2024-02-12 02:24:00 +0200

    add price to best seller item

---
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -0,0 +1,57 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utlis/assets.dart';
+import 'package:bookly_app/core/utlis/styles.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 130,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2 / 3,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(8),
+                image:const DecorationImage(image: AssetImage(AssetsData.testImage),fit: BoxFit.fill),
+              ),
+            ),
+          ),
+          const SizedBox(width: 30,),
+           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width *.5,
+                child: Text(
+                  'Harry Potter and the Golbet of Fire',
+                  style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  ),
+              ),
+
+              const SizedBox(
+                height: 3,
+              ),
+              const Text('J.K. Rowling', style: Styles.textStyle14,),
+              
+              const SizedBox(height: 3,),
+              Row(children: [
+                Text(
+                  '19.99 €',
+                  style: Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),
+                ),
+              ],)
+              ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
-import 'package:bookly_app/core/utlis/assets.dart';
 import 'package:bookly_app/core/utlis/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
@@ -27,45 +27,6 @@ class HomeViewBody extends StatelessWidget {
             height: 20,
           ),
           BestSellerListViewItem(),
-        ],
-      ),
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 130,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2 / 3,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(8),
-                image:const DecorationImage(image: AssetImage(AssetsData.testImage),fit: BoxFit.fill),
-              ),
-            ),
-          ),
-          const SizedBox(width: 30,),
-           Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width *.5,
-                child:const Text(
-                  'Harry Potter and the Golbet of Fire',
-                  style: Styles.textStyle20,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
-                  ),
-              ),
-              ],
-          ),
         ],
       ),
     );
