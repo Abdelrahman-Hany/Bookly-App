619c68f085e057fa00a6c04a0e72c21ec138b2c9
Abdelrahman-Hany <abdelrahman.hany013@gmail.com>
2024-02-13 15:49:01 +0200

    create book details custom app bar

--- lib/features/splash/presentation/views/widgets/splash_view.dart
+++ lib/features/splash/presentation/views/widgets/splash_view.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utlis/app_router.dart';
 import 'package:bookly_app/core/utlis/assets.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
@@ -60,7 +61,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
   void navigateToHome() {
     Future.delayed(const Duration(seconds: 2), () {
       // Get.to(() =>const HomeView(), transition: Transition.fadeIn,duration: kTranstionDuration);
-      GoRouter.of(context).push("/homeview");
+      GoRouter.of(context).push(AppRouter.kHomeView);
     });
   }
 }
